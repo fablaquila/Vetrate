9c141080c417b3e393dad72f571ef9a8c264ce39
pieroit <savastanos@libero.it>
2016-01-23 15:35:05 +0100

    load tiles successfully

--- src/Tile.scala
+++ src/Tile.scala
@@ -1,8 +1,9 @@
 /**
  * the Tile class
  */
-class Tile {
+class Tile( val w: Double, val h: Double ) {
 
-
+  val width:  Double = w
+  val height: Double = h
 
 }
--- src/Vetrate.scala
+++ src/Vetrate.scala
@@ -1,26 +1,51 @@
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * Punto di entrata nel programma
  */
 object Vetrate {
 
-  def loadTiles( filename: String ): Array[Tile] = {
+  def loadTiles( filename: String ): ArrayBuffer[Tile] = {
 
     // Lista di pezzi
-    var tiles = new Array[Tile](3)
+    var tiles = new ArrayBuffer[Tile](0)
 
     // carica file CSV
+    var numLines = 0
     val file = io.Source.fromFile(filename)
-    for( i <- file.getLines() ) {
-      println( i )
+    for( line <- file.getLines() ) {
+      numLines += 1
+
+      // slata la prima linea
+      if( numLines > 1 ) {
+
+        // estrai larghezza, altezza, quantità
+        val tileInfo = line.split(',')
+        val width    = tileInfo(0).toDouble
+        val height   = tileInfo(1).toDouble
+        val quantity = tileInfo(2).toInt
+
+        // costruisci il numero corretto di pezzi con le dimensioni richieste
+        for ( q <- 1 to quantity ) {
+          tiles += new Tile( width, height )
+        }
+      }
     }
 
     return tiles
   }
 
   def main( args: Array[String] ) {
+
+    // leggi il CSV e metti tutti i pezzi in un array
     var tiles = loadTiles("/home/piero/Desktop/progetti/Vetrate/src/vetri_fablab.csv")
-    println( tiles )
+
+    // verifica che ci sia tutto
+    println( "Caricate " + tiles.length + " tessere." )
+    for( t <- tiles ){
+      println( s"\t ${t.width}, ${t.height}" )
+    }
   }
 
 }
